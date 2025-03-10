f10c318eec37255b09419d89f6cec00559efb6b1
Mike-ferka <ferkamichaelgha@gmail.com>
2025-03-10 11:06:46 +0000

    Add Prisma client

--- app/api/tests/route.ts
+++ app/api/tests/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "../../../utils/prisma";
+import { prisma } from "../../utils/prisma";
 import { z } from "zod";
 
 const testSchema = z.object({
